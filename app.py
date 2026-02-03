ab1d327bfa2a4dac35ef442eb7c27d800bc9c098
GabrielMateo2003 <108085923+GabrielMateo2003@users.noreply.github.com>
2026-02-03 17:56:04 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,6 @@
+def predecir_inventario(ventas_actuales):
+    # Una lógica simple para probar tu flujo
+    return ventas_actuales * 1.1
+
+if __name__ == "__main__":
+    print(f"Predicción de inventario: {predecir_inventario(100)}")
